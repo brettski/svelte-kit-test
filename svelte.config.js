870a7b357e1ac3247c19c9486c4641b32e7dc3e9
Brett Slaski <brett@thatconference.com>
2022-02-07 21:10:01 -0600

    what a mess

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,18 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 /** @type {import('@sveltejs/kit').Config} */
 //import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
 		adapter: adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: '#svelte'
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	},
 
 	preprocess: [mdsvex(mdsvexConfig)]
